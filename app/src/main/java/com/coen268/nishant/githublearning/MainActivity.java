7754aebc4f762bf82e05d6fd5d700e2fff2e5b13
Nishant Phatangare <nishant1914@gmail.com>
2015-03-05 11:52:09 -0800

    Branch_1 commit

--- app/src/main/java/com/coen268/nishant/githublearning/MainActivity.java
+++ app/src/main/java/com/coen268/nishant/githublearning/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 // This will be edited in branch to see the changes//
 
 //This is written in Branch_1 //
+//This is written in Branch_1 //
 
 public class MainActivity extends ActionBarActivity {
 
